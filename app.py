8424115de101f788a1e831b4b25b850a593a177d
adamemcvey <64866324+adamemcvey@users.noreply.github.com>
2020-09-01 02:46:53 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import pymongo
+import scrape_mars
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+
+@app.route("/")
+def index():
+    mars_data = mongo.db.mars_db.find_one()
+    return render_template("index.html", mars_data=mars_data)
+
+@app.route("/scrape")
+def scraping():
+    mars_data = scrape_mars.scrape()
+    mongo.db.mars_db.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
